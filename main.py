08c32abc056d2ac6d021b0fe1ecde028021495a3
Osvaldo Jimenez <osjimene@microsoft.com>
2023-04-25 14:14:57 -0500

    Added the remaining data pull features  minus the Certificate puller

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import easygui
 import pandas as pd
 import openpyxl
-from modules.dataGrabber import get_filehash, get_filename
+from modules.dataGrabber import get_filehash, get_filename, get_internalname, get_filedescription,get_productname,get_fileversion
 import os
 import subprocess
 
@@ -12,9 +12,14 @@ file = easygui.fileopenbox(default=str())
 #Get Fileinformation
 filename = get_filename(file)
 filehash = get_filehash(file)
+internalname = get_internalname(file)
+fileversion = get_fileversion(file)
+filedescription = get_filedescription(file)
+productname = get_productname(file)
+
 
 #Parse collected data into a CSV format. 
 
 #Open the desired csv/excel and append the new information to excel
-print(filehash)
+print(f"Filename: {filename} \nFileHash: {filehash} \nInternalName: {internalname} \nVersion: {fileversion} \nFileDescription: {filedescription} \nProductName: {productname} ")
 #Save and close the program.
--- modules/dataGrabber.py
+++ modules/dataGrabber.py
@@ -12,3 +12,23 @@ def get_filehash(file):
     result = subprocess.run(["powershell", f"Get-FileHash -Path '{file}' -Algorith {hash_algo}"],capture_output=True, text=True)
     hash_value = result.stdout.split()[7]
     return hash_value
+
+def get_internalname(file):
+    result = subprocess.run(["powershell", f"(Get-Item '{file}').VersionInfo.InternalName"],capture_output=True, text=True)
+    internalname = result.stdout
+    return internalname
+
+def get_fileversion(file):
+    result = subprocess.run(["powershell", f"(Get-Item '{file}').VersionInfo.FileVersion"],capture_output=True, text=True)
+    fileversion = result.stdout
+    return fileversion
+
+def get_filedescription(file):
+    result = subprocess.run(["powershell", f"(Get-Item '{file}').VersionInfo.FileDescription"],capture_output=True, text=True)
+    filedescription = result.stdout
+    return filedescription
+
+def get_productname(file):
+    result = subprocess.run(["powershell", f"(Get-Item '{file}').VersionInfo.ProductName"],capture_output=True, text=True)
+    productname = result.stdout
+    return productname
